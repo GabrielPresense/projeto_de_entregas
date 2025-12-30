e2ffeba740b2042c9e69b0a4a4a44dc96299f857
Gabriel Presense <gabrielpresense@gmail.com>
2025-12-30 08:32:23 -0300

    ajustes de validação nos DTOs com campos opcionais

--- back/src/veiculos/dto/create-veiculo.dto.ts
+++ back/src/veiculos/dto/create-veiculo.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsEnum, IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TipoVeiculo } from '../veiculo.entity';
 
 export class CreateVeiculoDto {
@@ -20,6 +20,7 @@ export class CreateVeiculoDto {
   @IsInt()
   capacidade!: number;
 
+  @IsOptional()
   @IsBoolean()
   disponivel?: boolean;
 }
